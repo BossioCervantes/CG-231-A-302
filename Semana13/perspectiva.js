12848a698ffe2bab0c87226d06270599fd805006
BossioCervantes <124269531+BossioCervantes@users.noreply.github.com>
2023-04-27 20:47:24 -0500

    perspectiva.js

--- Semana13/perspectiva.js
+++ Semana13/perspectiva.js
@@ -1,7 +1,7 @@
-// Crear geometría de la pirámide
+// Se define geometría de la pirámide
 var geometry = new THREE.BufferGeometry();
 
-// Definir los vértices de la pirámide
+// Se definen los vértices de la pirámide
 var vertices = new Float32Array([
   // Triángulo base
   -1, 0, -1,
@@ -12,7 +12,7 @@ var vertices = new Float32Array([
   0, 1, 0
 ]);
 
-// Definir los índices de los triángulos de la pirámide
+// Se definen los índices de los triángulos de la pirámide
 var indices = new Uint16Array([
   0, 1, 2, // Cara 1
   0, 2, 3, // Cara 2
@@ -22,26 +22,26 @@ var indices = new Uint16Array([
   3, 4, 0  // Cara 6
 ]);
 
-// Agregar los vértices e índices a la geometría
+// Se agregan los vértices e índices a la geometría
 geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
 geometry.setIndex(new THREE.BufferAttribute(indices, 1));
 
-// Crear material
+//  material
 var material = new THREE.MeshBasicMaterial({ color: 0x00FFFF });
 
-// Crear malla de la pirámide
+//  malla de la pirámide
 var pyramid = new THREE.Mesh(geometry, material);
 
-// Crear escena y agregar la malla de la pirámide
+//  escena y agregar la malla de la pirámide
 var scene = new THREE.Scene();
 scene.add(pyramid);
 
-// Crear cámara de perspectiva
+// Se crea la cámara de perspectiva
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 camera.position.y = 2;
 
-// Crear renderizador y añadir a DOM
+// Se crea el renderizador 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);

a8702cd4f885aa36badf90e359dcc04ae0c51a7a
BossioCervantes <124269531+BossioCervantes@users.noreply.github.com>
2023-04-27 20:46:17 -0500

    ortogonal.js

--- Semana13/ortogonal.js
+++ Semana13/ortogonal.js
@@ -1,7 +1,6 @@
-// Crear geometría de la pirámide
 var geometry = new THREE.BufferGeometry();
 
-// Definir los vértices de la pirámide
+// Se definen los vértices de la pirámide
 var vertices = new Float32Array([
   // Triángulo base
   -1, 0, -1,
@@ -12,7 +11,7 @@ var vertices = new Float32Array([
   0, 1, 0
 ]);
 
-// Definir los índices de los triángulos de la pirámide
+// Se definen los indices de los triángulos de la pirámide
 var indices = new Uint16Array([
   0, 1, 2, // Cara 1
   0, 2, 3, // Cara 2
@@ -22,24 +21,24 @@ var indices = new Uint16Array([
   3, 4, 0  // Cara 6
 ]);
 
-// Agregar los vértices e índices a la geometría
+// Se agregan los vértices e índices a la geometría
 geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
 geometry.setIndex(new THREE.BufferAttribute(indices, 1));
 
-// Crear material
+// material
 var material = new THREE.MeshBasicMaterial({ color: 0x00FFFF });
 
-// Crear malla de la pirámide
+// malla de la pirámide
 var pyramid = new THREE.Mesh(geometry, material);
 var scene = new THREE.Scene();
 scene.add(pyramid);
 
-// Crear cámara ortográfica
+// cámara ortográfica
 var camera = new THREE.OrthographicCamera(window.innerWidth / -100, window.innerWidth / 100, window.innerHeight / 100, window.innerHeight / -100, 0.1, 100);
 camera.position.z = 5;
 camera.position.y = 2;
 
-// Crear renderizador y añadir a DOM
+// renderizador
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -48,7 +47,7 @@ document.body.appendChild(renderer.domElement);
 function animate() {
 requestAnimationFrame(animate);
 
-// Rotar la pirámide sobre el eje Y
+// Se rota la pirámide sobre el eje Y
 pyramid.rotation.y += 0.01;
 
 // Renderizar la escena con la cámara actual
